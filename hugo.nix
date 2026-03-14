c1b06df43e1c2d25cc2fdd3789581bd5a9f2694d
dvprokofiev <d@dvprokofiev.ru>
2026-03-14 16:15:38 +0300

    fix(webhook): incorrect Caddyfile

--- hugo.nix
+++ hugo.nix
@@ -40,10 +40,9 @@ in
         
         handle_path /webhook-deploy* {
           reverse_proxy localhost:9000
-          
-          header_up Host {host}
-          header_up X-Real-IP {remote_host}
-          header_up Content-Type {header.Content-Type}
+            header_up Host {host}
+            header_up X-Real-IP {remote_host}
+            header_up Content-Type {header.Content-Type}
         }
       '';
     };
